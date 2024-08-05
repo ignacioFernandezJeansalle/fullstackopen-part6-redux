3c5b7266177778adc3a8edcd50c5bb0e7d973fc4
Ignacio Fernandez Jeansalle <ignaciofj@gmail.com>
2024-08-05 14:12:31 -0300

    Create AnecdoteList component

---
+++ anecdotes/src/components/AnecdoteList.jsx
@@ -0,0 +1,32 @@
+import { useSelector, useDispatch } from "react-redux";
+import { addVote } from "../reducers/anecdoteReducer";
+import "./AnecdoteList.css";
+
+const AnecdoteListItem = ({ anecdote, handleClick }) => {
+  return (
+    <li className="anecdotes-list-item" key={anecdote.id}>
+      <p className="anecdotes-list-item-content">{anecdote.content}</p>
+      <div className="anecdotes-list-item-votes">
+        has: {anecdote.votes}
+        <button onClick={handleClick}>Vote</button>
+      </div>
+    </li>
+  );
+};
+
+const AnecdoteList = () => {
+  const anecdotes = useSelector((state) => state.toSorted((a, b) => b.votes - a.votes));
+  const dispatch = useDispatch();
+
+  return (
+    <section className="anecdotes-list-container">
+      <ul className="anecdotes-list">
+        {anecdotes.map((anecdote) => (
+          <AnecdoteListItem key={anecdote.id} anecdote={anecdote} handleClick={() => dispatch(addVote(anecdote.id))} />
+        ))}
+      </ul>
+    </section>
+  );
+};
+
+export default AnecdoteList;
