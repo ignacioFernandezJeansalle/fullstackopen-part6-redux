5e48ffae026337ce2d24dfb085a74273c93bef45
Ignacio Fernandez Jeansalle <ignaciofj@gmail.com>
2024-08-05 14:13:03 -0300

    Refactor with AnecdoteList component (ex6.8)

--- anecdotes/src/App.jsx
+++ anecdotes/src/App.jsx
@@ -1,38 +1,16 @@
-import { useSelector, useDispatch } from "react-redux";
-import { addVote } from "./reducers/anecdoteReducer";
+import AnecdoteList from "./components/AnecdoteList";
 import AnecdoteForm from "./components/AnecdoteForm";
 
 import "./App.css";
 
 const App = () => {
-  const anecdotes = useSelector((state) => state.toSorted((a, b) => b.votes - a.votes));
-
-  const dispatch = useDispatch();
-
-  const vote = (id) => {
-    dispatch(addVote(id));
-  };
-
   return (
     <>
       <header>
         <h1>Anecdotes</h1>
       </header>
       <main>
-        <section className="anecdotes">
-          <ul className="anecdotes-list">
-            {anecdotes.map((anecdote) => (
-              <li className="anecdotes-listitem" key={anecdote.id}>
-                <p className="anecdotes-content">{anecdote.content}</p>
-                <div className="anecdotes-votes">
-                  has: {anecdote.votes}
-                  <button onClick={() => vote(anecdote.id)}>Vote</button>
-                </div>
-              </li>
-            ))}
-          </ul>
-        </section>
-
+        <AnecdoteList />
         <AnecdoteForm />
       </main>
     </>
