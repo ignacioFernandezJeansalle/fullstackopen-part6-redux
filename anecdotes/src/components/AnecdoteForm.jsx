2b69d68a9184eeb36d81b971aff85e2918e37cf7
Ignacio Fernandez Jeansalle <ignaciofj@gmail.com>
2024-08-05 13:53:54 -0300

    Create component

---
+++ anecdotes/src/components/AnecdoteForm.jsx
@@ -0,0 +1,27 @@
+import { useDispatch } from "react-redux";
+import { createAnecdote } from "../reducers/anecdoteReducer";
+
+import "./AnecdoteForm.css";
+
+const AnecdoteForm = () => {
+  const dispatch = useDispatch();
+
+  const addAnecdote = (event) => {
+    event.preventDefault();
+    const content = event.target.anecdote.value;
+    dispatch(createAnecdote(content));
+    event.target.anecdote.value = "";
+  };
+
+  return (
+    <section className="anecdote-form-container">
+      <h2>Create new</h2>
+      <form className="anecdote-form" onSubmit={addAnecdote}>
+        <input name="anecdote" />
+        <button type="submit">Create</button>
+      </form>
+    </section>
+  );
+};
+
+export default AnecdoteForm;
