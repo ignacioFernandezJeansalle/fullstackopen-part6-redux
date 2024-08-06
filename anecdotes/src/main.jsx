3bab66b8c528bab9925bbc2e343f06339e057e93
Ignacio Fernandez Jeansalle <ignaciofj@gmail.com>
2024-08-06 11:08:04 -0300

    Create store with reduxjs toolkit

--- anecdotes/src/main.jsx
+++ anecdotes/src/main.jsx
@@ -1,20 +1,16 @@
 import ReactDOM from "react-dom/client";
-import { createStore, combineReducers } from "redux";
+import store from "./store/store";
 import { Provider } from "react-redux";
 import App from "./App";
-
-import anecdoteReducer from "./reducers/anecdoteReducer";
-import filterReducer from "./reducers/filterReducer";
-
-const reducer = combineReducers({
-  anecdotes: anecdoteReducer,
-  filter: filterReducer,
-});
-
-const store = createStore(reducer);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
     <App />
   </Provider>
 );
+
+/* 
+Cambia la definición del filter reducer y sus action creators para usar la función createSlice de Redux Toolkit.
+
+También, comienza a utilizar Redux DevTools para depurar el estado de la aplicación fácilmente.
+*/
