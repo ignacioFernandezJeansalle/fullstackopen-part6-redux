9541eaca3a8a03649f38b252504d9d294db85020
Ignacio Fernandez Jeansalle <ignaciofj@gmail.com>
2024-08-06 10:39:38 -0300

    Rename reducer

--- anecdotes/src/reducers/anecdoteReducer.js
+++ anecdotes/src/reducers/anecdoteReducer.js
@@ -27,7 +27,7 @@ export const createAnecdote = (content) => {
 
 const initialState = anecdotesAtStart.map(asObject);
 
-const reducer = (state = initialState, action) => {
+const anecdoteReducer = (state = initialState, action) => {
   switch (action.type) {
     case "VOTE": {
       const id = action.payload.id;
@@ -43,4 +43,4 @@ const reducer = (state = initialState, action) => {
   }
 };
 
-export default reducer;
+export default anecdoteReducer;
