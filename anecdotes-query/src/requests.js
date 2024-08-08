4e4f9dab9ea28f8f2c6972b912f5892d48cf981a
Ignacio Fernandez Jeansalle <ignaciofj@gmail.com>
2024-08-08 11:37:48 -0300

    Add createAnecdote function

--- anecdotes-query/src/requests.js
+++ anecdotes-query/src/requests.js
@@ -1,5 +1,14 @@
 import axios from "axios";
 
+const baseUrl = "http://localhost:3001/anecdotes";
+
 export const getAnecdotes = () => {
-  return axios.get("http://localhost:3001/anecdotes").then((response) => response.data);
+  return axios.get(baseUrl).then((response) => response.data);
+};
+
+export const createAnecdote = (content) => {
+  const id = (100000 * Math.random()).toFixed(0);
+  const newAnecdote = { content, id, votes: 0 };
+
+  return axios.post(baseUrl, newAnecdote).then((response) => response.data);
 };
