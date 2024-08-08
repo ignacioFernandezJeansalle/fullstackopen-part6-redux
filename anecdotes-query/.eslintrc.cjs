411427d19588ed0326c8ac1aca0eda3b52bc88c7
Ignacio Fernandez Jeansalle <ignaciofj@gmail.com>
2024-08-08 15:40:52 -0300

    Add rule react/prop-types: [0]

--- anecdotes-query/.eslintrc.cjs
+++ anecdotes-query/.eslintrc.cjs
@@ -14,5 +14,6 @@ module.exports = {
   rules: {
     "react-refresh/only-export-components": ["warn", { allowConstantExport: true }],
     "no-unused-vars": 0,
+    "react/prop-types": [0],
   },
 };
